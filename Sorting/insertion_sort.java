0c0d5fe8c6f9fcd2a581c2e33cf9d14545f897bd
SUMYAK JAIN <3456gaurav@gmail.com>
2020-02-22 23:21:31 +0530

    insertion sort

--- Sorting/insertion_sort.java
+++ Sorting/insertion_sort.java
@@ -1,1 +1,32 @@
+public class insertion_sort {
+    void sort(int arr[]){
+        int n=arr.length;//size of array i.e no. of objects
+        for(int i=1;i<n;i++){
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0&&arr[j]>key){//moving the elements ahead of key from 0 to i-1
+                arr[j+1]=arr[j];
+                j=j-1;
+            }
+            arr[j+1]=key;
+        }
+    }
+    void print(int arr[]){//print method to print sorted array
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]+" ");
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        
+    //main 
+    int arr[]={2,5,3,12,43,1};
+    insertion_sort obj=new insertion_sort();
+    obj.sort(arr);
+        System.out.println("sorted array is:");
+    obj.print(arr);
+    
+    }
+}
 
