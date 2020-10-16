c428148f078538a69df43b86c90239d2db294833
Ujjawal Singhal <67731656+thisisujjawal@users.noreply.github.com>
2020-10-16 14:23:20 +0530

    added Node class
    added basis node class needed in BST class;

--- BST.java
+++ BST.java
@@ -1,1 +1,13 @@
+public class BST<E>{
+  
+  Node<E> root;
+  private class Node<E>{
+    E data;
+    Node left, right;
+    Node(E data){
+      this.data = data;
+      left = right = null;
+    }
+  }
+}
 
