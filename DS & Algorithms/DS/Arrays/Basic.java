ce1d6f1177dbbce3eb32003f618e31daead89703
SUMYAK JAIN <3456gaurav@gmail.com>
2021-01-02 15:44:06 +0530

    Update and rename Q1.java to Basic.java

---
+++ DS & Algorithms/DS/Arrays/Basic.java
@@ -0,0 +1,17 @@
+// Basic Array 
+public class array1 {
+        public static void main(String args[]){
+        int arr[]=new int[5];
+         arr[0]=2;
+         arr[1]=4;
+         arr[2]=6;
+         arr[3]=8;
+         arr[4]=10;
+         for(int i=0; i<arr.length;i++){
+                 System.out.println(arr[i]);
+
+                }
+
+        }
+}
+
