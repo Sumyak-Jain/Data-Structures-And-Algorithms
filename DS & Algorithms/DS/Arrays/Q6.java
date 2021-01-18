19df51fb8355c9fe5eeb95031e2ca55e5750e7ee
SUMYAK JAIN <3456gaurav@gmail.com>
2021-01-18 14:58:21 +0530

    Update Q6.java

--- DS & Algorithms/DS/Arrays/Q6.java
+++ DS & Algorithms/DS/Arrays/Q6.java
@@ -1,2 +1,23 @@
+/**
+Ques:
+Given an array, rotate the array by one position in clock-wise direction.
+Example:
+Input:
+N = 5
+A[] = {1, 2, 3, 4, 5}
+Output:
+5 1 2 3 4
 
+Approach: store last element in a variable
+shift all elements by by one possition
+place last element in first possition
+
+**/
+void rotate(int arr[], int n)
+{
+   int x = arr[n-1], i;
+   for (i = n-1; i > 0; i--)
+      arr[i] = arr[i-1];
+   arr[0] = x;
+}
 
