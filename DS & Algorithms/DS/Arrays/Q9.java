e1d93c9c0df8e6c82df0a4c97cbb9245292231de
SUMYAK JAIN <3456gaurav@gmail.com>
2021-02-16 01:42:09 +0530

    Update Q9.java

--- DS & Algorithms/DS/Arrays/Q9.java
+++ DS & Algorithms/DS/Arrays/Q9.java
@@ -1,2 +1,13 @@
-
+// 287. Find the Duplicate Number (LeetCode)
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==nums[i+1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
 
