c362e6f0e7cac5bdde4d297e3ec34cd3e29cca08
SUMYAK JAIN <3456gaurav@gmail.com>
2021-03-08 14:11:07 +0530

    Create Buy_Sell.java

---
+++ LeetCode/Buy_Sell.java
@@ -0,0 +1,14 @@
+//Best Time to Buy and Sell Stock
+public class Solution {
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+}
