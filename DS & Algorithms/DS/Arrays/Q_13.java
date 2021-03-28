362e2e6801e46901fa1c21b126b7fef8be65fcec
SUMYAK JAIN <3456gaurav@gmail.com>
2021-03-29 01:51:49 +0530

    Update Q_13.java

--- DS & Algorithms/DS/Arrays/Q_13.java
+++ DS & Algorithms/DS/Arrays/Q_13.java
@@ -1,2 +1,20 @@
-ed
-
+//Array Subset of another array
+class Compute {
+    public String isSubset( long a1[], long a2[], long n, long m) {
+          int i = 0;
+        int j = 0;
+        for ( i = 0; i < m; i++) {
+            for (j = 0; j < n; j++)
+                if (a2[i] == a1[j])
+                    break;
+ 
+            if (j == n)
+                return "No";
+        }
+ 
+       
+        return "Yes";
+    
+        
+    }
+}
