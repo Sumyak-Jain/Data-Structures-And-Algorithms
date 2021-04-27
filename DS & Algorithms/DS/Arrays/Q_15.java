27eb4a228624797df0dc7102bf91248a610ce56b
SUMYAK JAIN <3456gaurav@gmail.com>
2021-04-27 14:31:34 +0530

    Update Q_15.java

--- DS & Algorithms/DS/Arrays/Q_15.java
+++ DS & Algorithms/DS/Arrays/Q_15.java
@@ -1,2 +1,21 @@
-
-c
+// Search a element in 3D Matrix
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        boolean check=false;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]==target){
+                    
+                    check=true;
+                    break;
+                    
+                }
+                
+            }
+        }
+        return check;
+        
+        
+        
+    }
+}
