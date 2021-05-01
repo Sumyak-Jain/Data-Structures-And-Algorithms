55d270c85db3d7a9b587ffcc4f6bd34311608f66
SUMYAK JAIN <3456gaurav@gmail.com>
2021-05-02 02:41:47 +0530

    Update string1.java

--- DS & Algorithms/DS/String/string1.java
+++ DS & Algorithms/DS/String/string1.java
@@ -1,1 +1,20 @@
+// check palindrome
 
+class Solution {
+    int isPlaindrome(String S) {
+        
+        String m="";
+        char[] ch = S.toCharArray();
+        int n=S.length()-1;
+       for(int i=n;i>=0;i--){
+            m=m+ch[i];
+        }
+        boolean ans = S.equals(m);
+        if(ans==true){
+            return 1;
+        }
+        else{
+        return 0;
+        }
+    }
+};
