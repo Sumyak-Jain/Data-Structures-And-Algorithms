4d3e1a02564d860fe5cf188b78aee6078cd1e660
SUMYAK JAIN <3456gaurav@gmail.com>
2021-05-22 02:35:09 +0530

    Update Q_2.java

--- DS & Algorithms/DS/String/Q_2.java
+++ DS & Algorithms/DS/String/Q_2.java
@@ -1,3 +1,24 @@
-
-g
-
+//Roman Number to Integer 
+class RomanToNumber {
+    // Finds decimal value of a given roman numeral
+    public int romanToDecimal(String str) {
+        
+        HashMap<Character, Integer> values = new HashMap<Character, Integer>();
+        values.put('I',1);
+    	values.put('V',5);
+    	values.put('X',10);
+    	values.put('L',50);
+    	values.put('C',100);
+    	values.put('D',500);
+    	values.put('M',1000);
+    	Integer value=0;
+    	char prev='M';
+    	for(char c:str.toCharArray()) {
+    			if(values.get(c)>values.get(prev))
+    				value=value-2*values.get(prev);
+    			value+=values.get(c);
+    			prev=c;
+    	}
+		return value;
+    }
+}
