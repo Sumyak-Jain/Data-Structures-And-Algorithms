217536a1a839a8f649ece74b97736e3e4ff9b342
SUMYAK JAIN <3456gaurav@gmail.com>
2021-06-21 02:55:45 +0530

    Update Square_root_integer.java

--- Interview Bit/Binary Search/Square_root_integer.java
+++ Interview Bit/Binary Search/Square_root_integer.java
@@ -1,3 +1,4 @@
+// first
 public class Solution {
     public int sqrt(int num) {
         
@@ -21,4 +22,26 @@ public class Solution {
     }
     return res ;
     }
+}
+
+//second
+public class Solution {
+   public int sqrt(int a) {
+	    long low = 1;
+	    long high = a;
+	    while (low<=high) {
+	        long mid = (high + low) / 2;
+	        if (mid*mid == a) {
+	            return (int) mid;
+	        }
+	        if (mid*mid > a) {
+	            high = mid - 1;
+	        } else {
+	            low = mid + 1;
+	        }
+	    }
+	    // if we did not find an exact match the high variable is smaller than low
+	    // and therefore contains the floor value of sqrt.
+	    return (int) high;
+	}
 }
