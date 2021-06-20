e7112deef43c214940c6b54137aa6bf83dd20344
SUMYAK JAIN <3456gaurav@gmail.com>
2021-06-21 02:23:05 +0530

    Update Search_for_range.java

--- Interview Bit/Binary Search/Search_for_range.java
+++ Interview Bit/Binary Search/Search_for_range.java
@@ -1,3 +1,20 @@
-
-
-n
+public class Solution {
+    public ArrayList<Integer> searchRange(final List<Integer> A, int B) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for(int i=0;i<A.size();i++){
+            if(A.get(i)==B){
+                arr.add(i);
+            }
+        }
+          ArrayList<Integer> arr1 = new ArrayList<Integer>();
+          if(arr.size()==0){
+              arr1.add(-1);
+               arr1.add(-1);
+              
+          }
+          else{
+          arr1.add(arr.get(0));
+          arr1.add(arr.get(arr.size()-1));}
+          return arr1;
+    }
+}
