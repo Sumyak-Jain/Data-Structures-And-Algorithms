07873565bf94e1d2afd59618b21cb9a81d6f717b
SUMYAK JAIN <3456gaurav@gmail.com>
2021-07-08 22:46:35 +0530

    Update Reverse_bits.java

--- Interview Bit/Bit Manupulation/Reverse_bits.java
+++ Interview Bit/Bit Manupulation/Reverse_bits.java
@@ -1,1 +1,21 @@
 
+public class Solution {
+    public long reverse(long a) {
+         long out = 0;
+
+
+            for(int i  = 0; i < 32; i++) {
+
+                out = out << 1L;
+               
+                if ((a & 1L) == 1L) {
+                        out += 1;
+                }
+
+                a = a >>> 1L;
+
+            }
+
+            return out;
+    }
+}
