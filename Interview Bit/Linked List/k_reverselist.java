15d7ab4231624c471a67153155c82ad05c62c21d
SUMYAK JAIN <3456gaurav@gmail.com>
2021-07-27 18:19:35 +0530

    Update k_reverselist.java

--- Interview Bit/Linked List/k_reverselist.java
+++ Interview Bit/Linked List/k_reverselist.java
@@ -1,1 +1,29 @@
 
+ public ListNode reverseList(ListNode A, int B) {
+        if(A == null)
+          return null;
+        ListNode current = A;
+        ListNode next = null;
+        ListNode prev = null;
+ 
+        int count = 0;
+ 
+        /* Reverse first k nodes of linked list */
+        while (count < B && current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+            count++;
+        }
+ 
+        /* next is now a pointer to (k+1)th node
+           Recursively call for the list starting from
+           current. And make rest of the list as next of
+           first node */
+        if (next != null)
+            A.next = reverseList(next, B);
+ 
+        // prev is now head of input list
+        return prev;
+     
