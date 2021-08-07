7a65e6d76a9247c4679f3227c3f47a67bac142b0
SUMYAK JAIN <3456gaurav@gmail.com>
2021-08-08 00:00:05 +0530

    Update Basic.java

--- DS & Algorithms/DS/Arrays/Basic.java
+++ DS & Algorithms/DS/Arrays/Basic.java
@@ -8,7 +8,7 @@ public class array1 {
          arr[3]=8;
          arr[4]=10;
          for(int i=0; i<arr.length;i++){
-                 System.out.println(arr[i]);
+                 System.out.println(arr[i]);v
 
                 }
 
