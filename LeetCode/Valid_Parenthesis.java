668eb47c40f8be18e67e3bf1afa5ad60eb60244a
SUMYAK JAIN <3456gaurav@gmail.com>
2021-10-25 14:37:04 +0530

    Update Valid_Parenthesis.java

--- LeetCode/Valid_Parenthesis.java
+++ LeetCode/Valid_Parenthesis.java
@@ -1,1 +1,51 @@
 
+class Solution {
+    public boolean isValid(String expr) {
+       // Using ArrayDeque is faster than using Stack class
+        Deque<Character> stack
+            = new ArrayDeque<Character>();
+ 
+        // Traversing the Expression
+        for (int i = 0; i < expr.length(); i++)
+        {
+            char x = expr.charAt(i);
+ 
+            if (x == '(' || x == '[' || x == '{')
+            {
+                // Push the element in the stack
+                stack.push(x);
+                continue;
+            }
+ 
+            // If current character is not opening
+            // bracket, then it must be closing. So stack
+            // cannot be empty at this point.
+            if (stack.isEmpty())
+                return false;
+            char check;
+            switch (x) {
+            case ')':
+                check = stack.pop();
+                if (check == '{' || check == '[')
+                    return false;
+                break;
+ 
+            case '}':
+                check = stack.pop();
+                if (check == '(' || check == '[')
+                    return false;
+                break;
+ 
+            case ']':
+                check = stack.pop();
+                if (check == '(' || check == '{')
+                    return false;
+                break;
+            }
+        }
+ 
+        // Check Empty Stack
+        return (stack.isEmpty());
+        
+    }
+}
